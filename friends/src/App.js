e959de690ce313cc1a6fa2a70727ccdd224c8b1d
omendez30 <omendez9@outlook.com>
2019-04-18 23:08:09 -0400

    added protected page, still working a few bugs out

--- friends/src/App.js
+++ friends/src/App.js
@@ -3,14 +3,21 @@ import React, { Component } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Login from "./components/Login";
+import PrivateRoute from "./components/PrivateRoute";
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div className="App">
-          <Link to="/login">Login</Link>
+          <div>
+            <Link to="/login">Login</Link>
+          </div>
+          <div>
+            <Link to="/protected">Protected Page </Link>
+          </div>
           <Route path="/login" component={Login} />
+          <PrivateRoute path="protected" />
         </div>
       </Router>
     );
